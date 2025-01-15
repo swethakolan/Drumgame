9cbcd42f710561252b6d0b44a1d5a8830b287fc9
swethakolan <161916617+swethakolan@users.noreply.github.com>
2025-01-15 15:21:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrumButtons;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonSound=this.innerHTML;
+    makeSound(buttonSound);
+   
+  })
+}
+
+document.addEventListener("keydown",function(event)
+{
+  makeSound(event.key);
+})
+
+function makeSound(key)
+{
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('kick-bass.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//     audio.play();
